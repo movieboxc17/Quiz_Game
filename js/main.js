b5e9ec6257bbb900b080ca1d2edf45cd130d4fc1
movieboxc17 <128537617+movieboxc17@users.noreply.github.com>
2024-10-15 19:59:14 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 // Initialize Firebase
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
+ const firebaseConfig = {
+    apiKey: "AIzaSyAnJ3yO-E75Saf9_u8KVWLQePy3RZyX-fI",
+    authDomain: "quiz-game-42366.firebaseapp.com",
+    projectId: "quiz-game-42366",
+    storageBucket: "quiz-game-42366.appspot.com",
+    messagingSenderId: "901178406784",
+    appId: "1:901178406784:web:583405d18656deed26b365",
+    measurementId: "G-JE6NWVFKKL"
+  };
 
 const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
